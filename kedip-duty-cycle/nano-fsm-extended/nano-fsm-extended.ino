ac9cee1c0941e07105a034bb380f00e20fcc6e2c
waskita <waskita@ee.itb.ac.id>
2022-10-10 21:55:11 +0700

    update lampu kedip extended FSM
    sesuaikan nama variable
    lengkapi logfile

--- kedip-duty-cycle/nano-fsm-extended/nano-fsm-extended.ino
+++ kedip-duty-cycle/nano-fsm-extended/nano-fsm-extended.ino
@@ -1,40 +1,49 @@
 #define ON    100
 #define OFF   101
-#define OUTPUT_LED 12
+#define OUTPUT_LED 3
 int state = ON;
+int count = 0;
 int output = 1;
-int counter = 0;
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(OUTPUT_LED, OUTPUT);
   Serial.begin(115200);
 
-  fsm_init();
+  fsm_init(&state, &output, &count);
+  Serial.print("state: ");
+  Serial.print(state);
+  Serial.print(" count: ");
+  Serial.print(count);
+  Serial.print(" output: ");
+  Serial.print( output);
+  Serial.println();
+
   delay(1000);
 }
 
-void fsm_init() {
+void fsm_init(int  *state, int *output, int *count) {
   state = ON;
-  output = 1;
+  *output = 1;
+  *count = 0;
 }
-void fsm(int  *state, int *output, int *counter) {
+void fsm(int  *state, int *output, int *count) {
   switch (*state) {
     case ON: {
         *state = OFF;
         *output = 0;
-        *counter = 0;
+        *count = 0;
         break;
       }
     case OFF: {
-        if (*counter == 9) {
+        if (*count == 9) {
           *state = ON;
           *output = 1;
-          *counter++;
+          *count++;
         } else {
           *state = OFF;
           *output = 0;
-          *counter = *counter + 1;
+          *count = *count + 1;
         }
         break;
       }
@@ -42,7 +51,8 @@ void fsm(int  *state, int *output, int *counter) {
 }
 
 void loop() {
-  fsm(&state, &output, &counter);
+
+  fsm(&state, &output, &count);
   if (output == 1) {
     digitalWrite(OUTPUT_LED, HIGH);
   } else {
@@ -50,8 +60,8 @@ void loop() {
   }
   Serial.print("state: ");
   Serial.print(state);
-  Serial.print(" counter: ");
-  Serial.print(counter);
+  Serial.print(" count: ");
+  Serial.print(count);
   Serial.print(" output: ");
   Serial.print( output);
   Serial.println();
