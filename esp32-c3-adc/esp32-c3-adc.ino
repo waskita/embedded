e621963b826c8f925302ec70f017f05299747fe5
waskita <waskita@ee.itb.ac.id>
2023-08-20 10:55:10 +0700

    ESP32 C3 & ESP32 ADC speed

---
+++ esp32-c3-adc/esp32-c3-adc.ino
@@ -0,0 +1,42 @@
+int counter;  // how many iterations
+int time_begin = 0;
+int time_end = 0;
+int duration;
+const int PIN_OUTPUT = 1;
+int sensorPin = 0;  // select the input pin for the potentiometer
+
+void setup() {
+  Serial.begin(115200);
+  counter = 0;
+  pinMode(PIN_OUTPUT, OUTPUT);
+  Serial.println("start benchmark");
+}
+//---------------------------------------------------
+void loop() {
+  int data = 0;
+  int value;  // angka yang ditulis
+  value = 0;  // minimum value
+
+  data = analogRead(sensorPin);
+  //  Serial.println(data);
+  //  delay(1000);
+  digitalWrite(PIN_OUTPUT, HIGH);
+
+  data = analogRead(sensorPin);
+  //Serial.println(data);
+  //delay(1000);
+  digitalWrite(PIN_OUTPUT, LOW);
+
+  counter = counter + 1;
+  if (counter >= 10000) {
+    float period;
+    int time_now = millis();
+    counter = 0;
+    duration = time_now - time_begin;
+    period = duration / 10000.0 / 2;
+    Serial.print("period (ms): ");
+    Serial.println(period);
+    //  prepare next round
+    time_begin = time_now;
+  }
+}
